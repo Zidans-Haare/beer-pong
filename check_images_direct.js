a9786137b642e67eb48e944853577b9f7850a0e6
htw hetzner <htw@hetzner.ubuntu>
2026-01-12 22:50:44 +0000

    chore: cleanup test scripts

--- check_images_direct.js
+++
@@ -1,6 +0,0 @@
-
-const Database = require('better-sqlite3');
-const db = new Database('dev.db');
-
-const rows = db.prepare("SELECT name, image FROM Player WHERE image IS NOT NULL LIMIT 5").all();
-console.log(JSON.stringify(rows, null, 2));

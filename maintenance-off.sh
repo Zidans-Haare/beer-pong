45915c5def2569d912fa33c20e9b93d1916f1205
NickMac <nick@olomek.com>
2026-03-08 10:39:08 +0100

    feat: Wartungsscreen zeigt optionale Info-Nachricht an
    maint-on fragt jetzt nach einer Nachricht (z.B. "neuer Build, commit abc123").
    Die Nachricht wird in maintenance-msg.txt gespeichert und per fetch() im
    Wartungsscreen angezeigt. maint-off räumt die Datei automatisch auf.

--- maintenance-off.sh
+++ maintenance-off.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Wartungsmodus deaktivieren
 rm -f /home/htw/beer-pong/public/maintenance.on
+rm -f /home/htw/beer-pong/public/maintenance-msg.txt
 echo "Wartungsmodus: AUS"
--- maintenance-on.sh
+++ maintenance-on.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Wartungsmodus aktivieren
+read -p "Was passiert gerade? (Enter für leer): " MSG
 touch /home/htw/beer-pong/public/maintenance.on
+if [ -n "$MSG" ]; then
+    echo "$MSG" > /home/htw/beer-pong/public/maintenance-msg.txt
+else
+    rm -f /home/htw/beer-pong/public/maintenance-msg.txt
+fi
 echo "Wartungsmodus: AN"

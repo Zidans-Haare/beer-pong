c53a805680b58c8785e869e56d4efd91d5d72c25
NickMac <nick@olomek.com>
2026-03-08 10:17:02 +0100

    feat: Under-Construction Seite + Wartungsmodus-Scripts
    Statische maintenance.html für Nginx-Wartungsmodus.
    maintenance-on.sh / maintenance-off.sh zum Umschalten.

---
+++ maintenance-off.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Wartungsmodus deaktivieren
+rm -f /var/www/beer-pong/maintenance.on
+echo "Wartungsmodus: AUS"
---
+++ maintenance-on.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Wartungsmodus aktivieren
+touch /var/www/beer-pong/maintenance.on
+echo "Wartungsmodus: AN"

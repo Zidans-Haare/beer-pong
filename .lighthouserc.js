8ba2b8806992ca25066c1c5c67a020146f142ec0
Hetzner <hetzner@local>
2026-04-20 16:48:16 +0000

    feat(lighthouse): add all app pages to CI checks (excl. admin/demo/offline) [skip ci]

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,8 +4,17 @@ module.exports = {
       url: [
         'https://bier.olomek.com/',
         'https://bier.olomek.com/players',
+        'https://bier.olomek.com/players/new',
         'https://bier.olomek.com/tournaments',
+        'https://bier.olomek.com/tournaments/new',
+        'https://bier.olomek.com/tournaments/archive',
         'https://bier.olomek.com/stats',
+        'https://bier.olomek.com/chat',
+        'https://bier.olomek.com/notifications',
+        'https://bier.olomek.com/settings',
+        'https://bier.olomek.com/streaming',
+        'https://bier.olomek.com/rules',
+        'https://bier.olomek.com/register',
       ],
       puppeteerScript: './lighthouse-auth.js',
       puppeteerLaunchOptions: {
